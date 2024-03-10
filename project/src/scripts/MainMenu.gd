18b54c79b52144230f571bb397b872a03f32f64a
David Vogel <david@davidvdev.com>
2024-03-09 23:37:29 -0600

    restructure. added level1 scene

---
+++ project/src/scripts/MainMenu.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+const GAME = preload("res://src/scenes/game_level_1.tscn")
+
+func _on_menu_play_pressed():
+	get_tree().change_scene_to_packed(GAME)
+	queue_free()
+
+func _on_menu_options_pressed():
+	var OPTIONS_MENU = load("res://src/scenes/options_menu.tscn").instantiate()
+	get_tree().root.add_child(OPTIONS_MENU)
+	queue_free()
+
+func _on_menu_quit_pressed():
+	get_tree().quit()
